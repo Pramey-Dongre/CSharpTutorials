4167625c50a1c38f6a1992227ca0d4e2b7b7c698
Pramey Dongre <pramey.dongre@ubisoft.com>
2024-08-02 16:00:05 +0530

    Encapsulation and Constructor

---
+++ HotelBooking/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HotelBooking
+{
+    class HotelBooking
+    {
+        private string _customerName;
+        private DateTime _date;
+        public HotelBooking(string customerName, DateTime date)
+        {
+            _customerName = customerName;
+            _date = date;
+        }
+        public HotelBooking(string customerName):
+            this(customerName,7)
+        {
+            Console.WriteLine("mein 1 args wala hoon");
+        }
+        public HotelBooking(string customerName, int daysFromNow)
+        {
+            _customerName = customerName;
+            _date = DateTime.Now.AddDays(daysFromNow);
+            Console.WriteLine("Mein toh 2 args waala hoon");
+        }
+
+        public void OverwriteDate(DateTime date)
+        {
+            _date = date;
+        }
+        public void OverwriteMonthAndDay(int month,int day)
+        {
+            _date = new DateTime(_date.Year, month, day);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{_customerName} has been scheduled for dining at {_date.Day}/{_date.Month}/{_date.Year} which is {_date.DayOfWeek}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hotelBooking = new HotelBooking("Grayson", new DateTime(2017, 8, 11));
+            hotelBooking.Display();
+            hotelBooking.OverwriteMonthAndDay(5, 20);
+            hotelBooking.Display();
+            hotelBooking.OverwriteDate(new DateTime(2031, 5, 12));
+            hotelBooking.Display();
+            var test = new HotelBooking("boooooyah",15);
+        }
+    }
+}
--- oops/Program.cs
+++ oops/Program.cs
@@ -8,26 +8,31 @@ namespace oops
         public int ParallelSide_2;
         public int Height;
 
+        public Trapezium():
+            this(-5,10,13)
+        {
+        }
+
         public Trapezium(int p1, int p2, int h)
         {
             ParallelSide_1 = p1;
             ParallelSide_2 = p2;
             Height = h;
         }
+
     }
     class ShapesMeasurementsCalculator
     {
-        public double CalculateTrapeziumArea(Trapezium trapezium)
-        {
-            return 0.5 * (trapezium.ParallelSide_1 + trapezium.ParallelSide_2) * trapezium.Height;
-        }
+        public double CalculateTrapeziumArea(Trapezium trapezium) => 0.5 * (trapezium.ParallelSide_1 + trapezium.ParallelSide_2) * trapezium.Height;
+        
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var trapezium = new Trapezium(19, 11, 17);
+            //var trapezium = new Trapezium(19, 11, 17);
+            var trapezium = new Trapezium();
             var calculator = new ShapesMeasurementsCalculator();
             Console.WriteLine(trapezium.ParallelSide_1);
             Console.WriteLine(trapezium.ParallelSide_2);
